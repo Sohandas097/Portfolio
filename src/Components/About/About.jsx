d0120ed0520023cd68e4c8f3a4aec0324aac4076
Sohan Das <www.sohandas097@gmail.com>
2024-09-06 22:12:00 +0530

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./About.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import profile_img from "../../assets/about_profile.svg";
+import sideprofile from "../../assets/sidepic.png";
+
+
+const About = () => {
+  return (
+    <div id="about" className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={sideprofile} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis, voluptatum?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+              sit.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>React Js</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Javascript</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Node Js</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Stats of achievements */}
+      {/* <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import profile_img from "../../assets/ppcir.png";
+import "./Hero.css";
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import resume from '../../assets/resume.pdf'
+
+const Hero = () => {
+  return (
+    <div id='home' className="hero">
+      <img src={profile_img} alt="" />
+      <h1><span>I'm Sohan Das,</span> full stack web developer based in India.</h1>
+      <p>
+        I am excited to start my career in this field. My skills in programming,
+        DSA, and web development, along with my eagerness to learn and adapt,
+        will help me contribute effectively.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
+        <a className="hero-resume" href={resume} download="resume" target='_blank'>Resume</a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
